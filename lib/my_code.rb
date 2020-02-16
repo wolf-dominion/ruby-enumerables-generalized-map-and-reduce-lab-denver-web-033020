af32a64c83fca6dfebddf614745a18cacdcbbb00
Hannah Drury <hannah_drury@yahoo.com>
2020-02-16 01:41:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
-# Your Code Here
+
+def map(array)
+  
+  new = []
+  count = 0
+  
+  while count < array.length
+    new.push(yield(array[count]))
+    count += 1
+  end
+  new
+end
+
+def reduce(array, sv=nil)
+  count = 1
+  total = 0
+  
+  if sv
+    total =  sv
+    count = 0
+  else
+    total = array[0]
+  end
+
+  while count < array.length
+    total = (yield(total, array[count]))
+    count += 1
+  end
+  total
+end
