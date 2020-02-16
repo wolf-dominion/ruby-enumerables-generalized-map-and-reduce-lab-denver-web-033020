896ede9ec5f6c0717a9495b2ea524018ffd1d3be
Hannah Drury <hannah_drury@yahoo.com>
2020-02-16 01:36:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,13 +21,9 @@ def reduce(array, sv=nil)
     total = 0
   end
   
-  truthyTest = false
   count = 0
   
   while count < array.length
-    if array[count] == true
-      truthyTest = true
-    end
     total = (yield(total, array[count]))
     count += 1
   end
