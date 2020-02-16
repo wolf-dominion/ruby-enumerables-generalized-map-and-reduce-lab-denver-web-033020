8885b63f0600664e3f53774a2ac4d9f4147af10e
Hannah Drury <hannah_drury@yahoo.com>
2020-02-16 01:01:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+
+def map(array)
+  
+  new = []
+  count = 0
+  
+  while count < array.length
+    new.push(yield(array[count]))
+    count += 1
+  end
+  new
+end
+
+def reduce(array, sv=nil)
+  
+  total = 0
+  count = 0
+  
+  while count < array.length
+    total += (yield(array))
+    count += 1
+  end
+  total
+end
